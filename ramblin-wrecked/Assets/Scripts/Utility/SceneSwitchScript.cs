5a31ad9932d0dd75eddf770f20f6274c1e038a02
zipdrive <phillips4271@gmail.com>
2018-10-16 09:34:21 -0400

    Main Menu
    Created a main menu.

---
+++ ramblin-wrecked/Assets/Scripts/Utility/SceneSwitchScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitchScript : MonoBehaviour {
+
+	public void SwitchToMainMenu()
+    {
+        SceneManager.LoadScene("Scenes/MainMenu");
+    }
+
+    public void SwitchToGame()
+    {
+        SceneManager.LoadScene("Scenes/MainGame");
+    }
+
+}
