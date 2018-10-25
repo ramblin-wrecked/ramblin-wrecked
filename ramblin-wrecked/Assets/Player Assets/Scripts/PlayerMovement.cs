85bd680d356391dd7ef3b31fab2ceacdb348e674
Nkosi Kee <nkosi.kee@gatech.edu>
2018-10-25 12:47:19 -0400

    Updated the Player Character
    The player character can now run at different speeds by holding shift and jumps at a consistent height.

--- ramblin-wrecked/Assets/Player Assets/Scripts/PlayerMovement.cs
+++ ramblin-wrecked/Assets/Player Assets/Scripts/PlayerMovement.cs
@@ -2,48 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody), typeof(CapsuleCollider))]
 public class PlayerMovement : MonoBehaviour {
 
-    float smallJump = 6f;
-    public float bigJump = 12f;
+    //Verticle Movment Variables
+    public float jumpVel = 12f;
+    public float fallMultiplier = 2.5f;
+    public bool isGrounded;
+
+    //Horizontal Movement Variables
     public float walkSpeed = 15f;
     public float maxSpeed = 30f;
     public float friction = 0.90f;
     public float velocity = 0f;
-    public float acceleration = 1f;
+
+
     Quaternion dirQuaternion;
     Vector3 dirVector;
     Rigidbody rigidbody;
     CharacterController charCtrl;
+    Collider charCollider;
     bool isRunning;
+
 
     void Awake()
     {
         rigidbody = GetComponent<Rigidbody>();
         charCtrl = GetComponent<CharacterController>();
+        charCollider = GetComponent<CapsuleCollider>();
+        isGrounded = false;
     }
 
-    void FixedUpdate()
+    void Update()
     {
         dirVector.Set(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        Vector3.Normalize(dirVector);
         Point(dirVector);
-        Move(dirVector);
-        rigidbody.AddForce(Physics.gravity * rigidbody.mass);
-        jump(Input.GetButtonDown("Jump"));
+        Move(dirVector, Input.GetButton("Fire3"));
+        jump("Jump");
+    }
+    void FixedUpdate()
+    {
+
     }
 
     //Moves the player
-    void Move (Vector3 dirVector)
+    void Move (Vector3 dirVector, bool running)
     {
-        rigidbody.position += transform.forward * Time.deltaTime * velocity;
-        if (dirVector.magnitude > 0)
-        {
-            if (velocity < maxSpeed) velocity += acceleration;
-            else velocity = maxSpeed;
+        bool shouldMove = dirVector.magnitude > 0f;
+        if (isGrounded){
+            rigidbody.position += transform.forward * Time.deltaTime * velocity;
+
         } else {
-            if (velocity > 0.5f) velocity *= friction;
-            else velocity = 0f;
+            rigidbody.position += dirVector * Time.deltaTime * velocity;
         }
+
+        if (shouldMove) {
+            if (running) velocity = maxSpeed;
+            else velocity = walkSpeed;
+        }
+        if (velocity > 0.5f) {
+            velocity *= friction;
+        }
+        else velocity = 0f;
     }
 
     //Points the player
@@ -55,11 +76,48 @@ public class PlayerMovement : MonoBehaviour {
             rigidbody.MoveRotation(dirQuaternion);
         }
     }
-    void jump(bool jump)
+
+    //Makes the player jump
+    void jump(string input)
     {
-        if(jump)
+        if(Input.GetButtonDown(input))
         {
-            rigidbody.AddForce(Vector3.up * bigJump);
+            if (isGrounded)
+            {
+                rigidbody.AddForce(Vector3.up * jumpVel, ForceMode.Impulse);
+                isGrounded = false;
+            }
+        }
+        if(rigidbody.velocity.y < 0)
+        {
+            rigidbody.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
         }
     }
+
+    //Physics Callback
+    void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject.tag =="Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    //Physics Callback
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+
 }
