335df18f8343d6b92adca9a941623ad4647a180a
ElienDiaz <eliericardo@hotmail.com>
2018-10-25 10:37:59 -0400

    Added Wall Prefab

---
+++ ramblin-wrecked/Assets/Scripts/MoveForward.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour {
+
+    Vector3 movement;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        movement = new Vector3(1, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = transform.position + movement * speed;
+	}
+}
