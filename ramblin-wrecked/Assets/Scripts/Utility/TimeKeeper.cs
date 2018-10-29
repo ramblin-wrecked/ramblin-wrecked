20148b4d4a36570cb60fe814542bb07bf23e1a02
zipdrive <phillips4271@gmail.com>
2018-10-29 10:04:21 -0400

    GameOver fixed
    Fixed the GameOver screen.

---
+++ ramblin-wrecked/Assets/Scripts/Utility/TimeKeeper.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeKeeper : MonoBehaviour {
+
+    public static float time;
+    public static bool notPaused;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (TimeKeeper.notPaused)
+            TimeKeeper.time += Time.deltaTime;
+	}
+}
