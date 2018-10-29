74e4cdca7c1803f99d66f63e6ce3f4bd176176bf
Courtney Rogers <crogers60@gatech.edu>
2018-10-28 23:26:23 -0400

    Adding Cup Stuff
    -Added an instance to the game. Will repeat in a sec

---
+++ ramblin-wrecked/Assets/Scripts/CheckCup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckCup : MonoBehaviour {
+
+    public bool hitCup = false;
+
+
+    public void isHit() { hitCup = true; }
+
+    public void outOfBox() { hitCup = false; }
+}
---
+++ ramblin-wrecked/Assets/Scripts/CupThrow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CupThrow : MonoBehaviour {
+
+    private GameObject Cuppy;
+    void Start()
+    {
+        Cuppy = this.transform.GetChild(0).gameObject; //Cup obj not default
+        Cuppy.gameObject.SetActive(false);
+
+    }
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.attachedRigidbody != null)
+        {
+            Cuppy.gameObject.SetActive(true);
+            Cuppy.GetComponent<Animator>().SetTrigger("Throw");
+            
+        }
+
+    }
+
+    private void OnTriggerExit(Collider c)
+    {
+        if (c.attachedRigidbody != null)
+        {
+            Cuppy.GetComponent<Animator>().SetTrigger("EndThrow");
+            Cuppy.gameObject.SetActive(false);
+
+            CheckCup bc = c.attachedRigidbody.gameObject.GetComponent<CheckCup>();
+            if (bc != null)
+            {
+                bc.outOfBox();
+                GameObject def = Cuppy.transform.GetChild(0).gameObject;
+                def.gameObject.SetActive(true);
+            }
+        }
+    }
+}
