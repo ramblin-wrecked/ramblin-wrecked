86241dfd83517f5b53604fefb5d9a6f0308088fa
ElienDiaz <eliericardo@hotmail.com>
2018-10-29 00:21:12 -0400

    Forgot to add a couple of files in previous commit

---
+++ ramblin-wrecked/Assets/Scripts/WallCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class WallCollision : MonoBehaviour
+{
+
+    private BoxCollider bc;
+    public bool GameOver = false;
+    void Awake()
+    {
+        bc = GetComponent<BoxCollider>();
+
+        if (bc == null)
+        {
+            Debug.Log("Box Collider could not be found");
+        }
+    }
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.tag == "Player")
+        {
+            print("Got Here");
+            GameOver = true;
+        }
+    }
+}
