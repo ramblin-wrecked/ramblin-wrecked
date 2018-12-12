e3a293d1ee925ab817938e9f5a02b15a316ad708
Courtney Rogers <crogers60@gatech.edu>
2018-12-11 23:50:39 -0500

    Book Stacking

---
+++ ramblin-wrecked/Assets/stackBooks.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stackBooks : MonoBehaviour {
+    public GameObject target;
+    public GameObject[] books = new GameObject[4];
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+        if (target == null)
+        {
+            Debug.Log("Player not found");
+        }
+    }
+
+    private void Update()
+    {
+        FixedPlayerMovement bc = target.gameObject.GetComponent<FixedPlayerMovement>();
+        int bookAm = bc.booksNum;
+        if(bookAm > 0)
+        {
+            for(int i = 0; i < bookAm; i++)
+            {
+                books[i]
+            }
+        }
+    }
+}
