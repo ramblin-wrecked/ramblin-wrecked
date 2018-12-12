2d7cbbeff979bb95903370fff69f29ce5a59a20c
zipdrive <phillips4271@gmail.com>
2018-12-11 21:20:03 -0500

    Status Effects UI
    Added status effects in the UI.

---
+++ ramblin-wrecked/Assets/Scripts/UI/BookStatusScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BookStatusScript : MonoBehaviour {
+
+    public Image bookImagePrefab;
+
+    public void AddBook()
+    {
+        Instantiate(bookImagePrefab, transform);
+    }
+
+    public void RemoveBook()
+    {
+        Image i = GetComponentInChildren<Image>();
+        if (i != null)
+            Destroy(i.gameObject);
+    }
+}
